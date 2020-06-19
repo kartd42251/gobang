af87a750af41a89db10645ed622e8f8fdda72df1
kartd42251 <43086548+kartd42251@users.noreply.github.com>
2020-06-19 19:56:36 +0800

    alter bug

--- robot_lib/robot_jeffrey.py
+++ robot_lib/robot_jeffrey.py
@@ -1,7 +1,7 @@
 import random 
 from game_basic import *
-dic = {(4,0):100,(4,1):100,(3,0):70,(3,1):4,(2,0):20,(2,1):2,(1,0):5,(0,0):0,(0,1):0,(1,1):0,}
-dic2 = {(4,0):200,(4,1):150,(3,0):70,(3,1):15,(2,0):20,(2,1):2,(1,0):5,(0,0):0,(0,1):0,(1,1):0,}
+dic = {(4,0):100,(4,1):120,(3,0):70,(3,1):15,(2,0):20,(2,1):2,(1,0):5,(0,0):0,(0,1):0,(1,1):0,}
+dic2 = {(4,0):200,(4,1):150,(3,0):90,(3,1):15,(2,0):20,(2,1):2,(1,0):5,(0,0):0,(0,1):0,(1,1):0,}
 
 def random_init(array2D):
     for i in range(30):
@@ -27,10 +27,10 @@ def eva3(array2D,size):
         for j in range(0,size):
             if(array2D[i][j] == '.'):
                 eva_result[i][j] =  max(eva_defence(array2D,i,j),eva_attack(array2D,i,j))
-    # for i in range(0,size):
-    #     for j in range(0,size):
-    #         print("{0:^3d}".format(eva_result[i][j]), end = "")
-    #     print()
+    for i in range(0,size):
+        for j in range(0,size):
+            print("{0:^3d}".format(eva_result[i][j]), end = "")
+        print()
     for i in range(0,size):
         for j in range(0,size):
             if(eva_result[i][j] > _max):
@@ -55,31 +55,31 @@ def eva_attack(array2D,x,y):
             if(array2D[x][y+k]!='O'):
                 ct_h += k-1
                 if(array2D[x][y+k]=='.'): 
-                    Sum_h +=  dic.get((k-1,0))
+                    Sum_h +=  dic2.get((k-1,0))
                     cnt_h += k-1
                     stupid_h += 100
                 elif(array2D[x][y+k]=='J'): 
                     stupid_h += k
                 else:
-                    Sum_h += dic.get((k-1,1))
+                    Sum_h += dic2.get((k-1,1))
                     stupid_h += k-1   
                 break
         for k in range(1,6): 
             if(array2D[x][y-k]!='O'): 
                 ct_h += k-1
                 if(array2D[x][y-k]=='.'): 
-                    Sum_h += dic.get((k-1,0))
+                    Sum_h += dic2.get((k-1,0))
                     cnt_h += k-1
                     stupid_h += 100
                 elif(array2D[x][y-k]=='J'): 
                     stupid_h += k
                 else:
-                    Sum_h += dic.get((k-1,1))
+                    Sum_h += dic2.get((k-1,1))
                     stupid_h += k-1 
                 if(ct_h>=4):
-                    Sum_h = dic.get((4,1))
+                    Sum_h = dic2.get((4,1))
                 elif(cnt_h==3):
-                    Sum_h = dic.get((3,0))
+                    Sum_h = dic2.get((3,0))
                 if(stupid_h <5):
                     Sum_h = 0
                 break
@@ -87,31 +87,31 @@ def eva_attack(array2D,x,y):
             if(array2D[x+k][y]!='O'): 
                 ct_a += k-1
                 if(array2D[x+k][y]=='.'): 
-                    Sum_a += dic.get((k-1,0))
+                    Sum_a += dic2.get((k-1,0))
                     cnt_a += k-1
                     stupid_a += 100
                 elif(array2D[x+k][y]=='J'): 
                     stupid_a += k 
                 else:
-                    Sum_a += dic.get((k-1,1)) 
+                    Sum_a += dic2.get((k-1,1)) 
                     stupid_a += k-1    
                 break
         for k in range(1,6):        
             if(array2D[x-k][y]!='O'): 
                 ct_a += k-1
                 if(array2D[x-k][y]=='.'): 
-                    Sum_a += dic.get((k-1,0))
+                    Sum_a += dic2.get((k-1,0))
                     cnt_a += k-1
                     stupid_a += 100
                 elif(array2D[x-k][y]=='J'): 
                     stupid_a += k 
                 else:
-                    Sum_a += dic.get((k-1,1))
+                    Sum_a += dic2.get((k-1,1))
                     stupid_a += k-1 
                 if(ct_a>=4):
-                    Sum_a = dic.get((4,1)) 
+                    Sum_a = dic2.get((4,1)) 
                 elif(cnt_a==3):
-                    Sum_a = dic.get((3,0))    
+                    Sum_a = dic2.get((3,0))    
                 if(stupid_a<5):
                     Sum_a = 0  
                 break
@@ -119,30 +119,30 @@ def eva_attack(array2D,x,y):
             if(array2D[x+k][y+k]!='O'): 
                 if(array2D[x+k][y+k]=='.'):
                     ct_l += k-1 
-                    Sum_l += dic.get((k-1,0))
+                    Sum_l += dic2.get((k-1,0))
                     stupid_l += 100
                 elif(array2D[x+k][y+k]=='J'):
                     stupid_l += k
                 else:
-                    Sum_l += dic.get((k-1,1)) 
+                    Sum_l += dic2.get((k-1,1)) 
                     stupid_l += k-1     
                 break
         for k in range(1,6):        
             if(array2D[x-k][y-k]!='O'):
                 ct_l += k-1 
                 if(array2D[x-k][y-k]=='.'): 
-                    Sum_l += dic.get((k-1,0))
+                    Sum_l += dic2.get((k-1,0))
                     cnt_l += k-1
                     stupid_l += 100
                 elif(array2D[x-k][y-k]=='J'):
                     stupid_l += k
                 else:
-                    Sum_l += dic.get((k-1,1))
+                    Sum_l += dic2.get((k-1,1))
                     stupid_l += k-1 
                 if(ct_l>=4):
-                    Sum_l = dic.get((4,1))   
+                    Sum_l = dic2.get((4,1))   
                 elif(cnt_l==3):
-                    Sum_l = dic.get((3,0)) 
+                    Sum_l = dic2.get((3,0)) 
                 if(stupid_l<5):
                     Sum_l = 0 
                 break
@@ -150,31 +150,31 @@ def eva_attack(array2D,x,y):
             if(array2D[x+k][y-k]!='O'):
                 ct_r += k-1 
                 if(array2D[x+k][y-k]=='.'): 
-                    Sum_r += dic.get((k-1,0))
+                    Sum_r += dic2.get((k-1,0))
                     cnt_r += k-1
                     stupid_r += 100
                 elif(array2D[x+k][y-k]=='J'):
                     stupid_r += k  
                 else:
-                    Sum_r += dic.get((k-1,1))  
+                    Sum_r += dic2.get((k-1,1))  
                     stupid_r += k-1   
                 break
         for k in range(1,6):        
             if(array2D[x-k][y+k]!='O'): 
                 ct_r += k-1
                 if(array2D[x-k][y+k]=='.'): 
-                    Sum_r += dic.get((k-1,0))
+                    Sum_r += dic2.get((k-1,0))
                     cnt_r += k-1
                     stupid_r += 100
                 elif(array2D[x-k][y+k]=='J'):
                     stupid_r += k 
                 else:
-                    Sum_r += dic.get((k-1,1))
+                    Sum_r += dic2.get((k-1,1))
                     stupid_r += k-1   
                 if(ct_r>=4):
-                    Sum_r = dic.get((4,1))
+                    Sum_r = dic2.get((4,1))
                 elif(cnt_r==3):
-                    Sum_r = dic.get((3,0))
+                    Sum_r = dic2.get((3,0))
                     if(stupid_r<5):
                         Sum_r = 0
                 break
